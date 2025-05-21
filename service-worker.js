e4817c4e21e1373a98eb053fb7158ff91fd13e76
skd6818 <kumarsoumen@rediffmail.com>
2025-05-21 13:37:58 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('b2b-b2c-cache').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
